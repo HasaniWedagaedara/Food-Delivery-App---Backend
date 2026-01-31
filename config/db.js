e8092ebcb43e0ffbca003a41bbc0907c3f7dd3e5
Hasani Madhumali <hasaniwedagedara123@gmail.com>
2026-01-31 11:39:25 +0530

    functions updated

--- config/db.js
+++ config/db.js
@@ -3,8 +3,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     await mongoose
-      .connect(
-        'mongodb+srv://hasanimadhumali:19991213@cluster0.hto3j3r.mongodb.net/food-delivery-app')
+      .connect(process.env.DATABASE_URL)
       .then(() => console.log("DB connected"));
 }
 
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
+import userRouter from './routes/userRoute.js';
+import dotenv from "dotenv";
+
+dotenv.config(); 
 
 // app config
 const app = express();
@@ -18,6 +22,7 @@ connectDB();
 
   app.use('/api/food', foodRouter);
   app.use('/images',express.static('uploads'));
+  app.use('/api/user',userRouter)
 
 
 app.get('/', (req, res) => {
